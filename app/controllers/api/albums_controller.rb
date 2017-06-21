4db8c769e355f732ddd9cee7f9267d996b0ba300
Juan Carlos <juanjcsr@gmail.com>
2017-06-20 19:07:10 -0500

    add index route to albums

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -1,2 +1,8 @@
 class Api::AlbumsController < ApplicationController
+  before_action :set_album, only: [:show, :update, :destroy]
+
+  def index
+    @albums = Album.all
+    json_response(@albums)
+  end
 end
